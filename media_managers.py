c1e6fd6b5bd1919ec4ebf2195f2c6871b623787b
Dima Smirnov <diamond101001@gmail.com>
2023-06-02 18:56:47 +0300

    update media_managers.py redesigned class structure. update utils.py changes related to the structure change. general refactoring has been performed

--- media_managers.py
+++ media_managers.py
@@ -1,82 +1,134 @@
 import random
 from pony.orm import db_session, select, commit
-from models import PhotoMem, TikTok, Greeting
+from models import *
 
 
 class MediaManager:
+    """
+    Базовый класс медиа менеджера.
+    """
+
+    model = None  # модель бд, с которой работает менеджер
+    attr_name = None  # название атрибута, который возвращает метод создания объекта из записи дб
+    __model_fields = []  # поля модели бд (заполняется автоматически)
+
+    def __init__(self):
+        if any(attr is None for attr in [self.model, self.attr_name]):
+            raise Exception("Обязательные атрибуты класса не заполнены!")
+
+        self.__model_fields = db.entities.get(self.model.__name__)._columns_
 
     @db_session
-    def active_false(self, obj):
-        obj.is_active = False
+    def active_false(self, object):
+        """
+        Метод деактивации объекта.
+        :param object: объект из бд.
+        """
+        object.is_active = False
         commit()
+
+    @db_session
+    def get_random_objects_from_db(self):
+        """
+        Метод получения случайного объекта из бд.
+        :return: объект, количество активных объектов оставшихся в базе.
+        """
+        objects = self.get_free_objects_from_db()
+        if not objects:
+            return None, 0
+
+        return random.choice(objects), len(objects) - 1
+
+    @db_session
+    def get_free_objects_from_db(self):
+        """
+        Метод получения списка активных объектов из бд.
+        :return: список активных объектов из бд.
+        """
+        if not self.model:
+            return False
+
+        return list(select(photo for photo in self.model if photo.is_active))
+
+    @db_session
+    def create_object_from_db_model(self):
+        """
+        Метод создания объекта из записи дб.
+        :return: объект, количество объектов, которые остались в базе.
+        """
+
+        if self.attr_name is None:
+            return None, 0
+
+        object, objects_count = self.get_random_objects_from_db()
+        if object is None:
+            return None, 0
+
+        self.active_false(object)
+        return getattr(object, self.attr_name), objects_count
+
+    @db_session
+    def create_model(self, data: dict):
+        """
+        Метод создания записи в бд.
+        :param data: данные для записи.
+        :return: объект созданной записи.
+        """
+        if not data or data.keys() != self.__model_fields:
+            return None
+
+        return self.model(**data)
 
 
 class ImageManager(MediaManager):
+    """
+    Класс менеджер изображений.
+    """
+    model = PhotoMem
+    attr_name = "link"
 
     @db_session
-    def get_random_image_from_db(self):
-        photos = self.get_free_images_from_db()
-        return random.choice(photos), len(photos) - 1
-
-    @db_session
-    def get_free_images_from_db(self):
-        return list(select(photo for photo in PhotoMem if photo.is_active))
-
-    @db_session
-    def images_model_create(self, images_data):
-        return [PhotoMem(link=image.url, width=image.width, height=image.height, is_active=True)
+    def image_models_create(self, images_data):
+        """
+        Метод создания новой записи изображения в бд.
+        :param images_data: список изображений.
+        :return: список записей изображений.
+        """
+        return [self.create_model({"link": image.link, "width": image.width, "height": image.height, "is_active": True})
                 for image in images_data]
-
-    @db_session
-    def create_image_from_db_model(self):
-        model, images_count = self.get_random_image_from_db()
-        self.active_false(model)
-
-        return model.link, images_count
 
 
 class TikTokManager(MediaManager):
+    """
+    Класс менеджер тик токов.
+    """
+    model = TikTok
+    attr_name = "link"
 
     @db_session
-    def get_random_tt_from_db(self):
-        tts = self.get_free_tt_from_db()
-        return random.choice(tts), len(tts) - 1
+    def tt_models_create(self, tt_data):
+        """
+        Метод создания новой записи видео в дб.
+        :param tt_data: список видео.
+        :return: список моделей видео из бд.
+        """
 
-    @db_session
-    def get_free_tt_from_db(self):
-        return list(select(tt for tt in TikTok if tt.is_active))
-
-    @db_session
-    def tt_model_create(self, tt_data):
         if tt_data:
-            return [TikTok(link=tt, is_active=True) for tt in tt_data]
-
-    @db_session
-    def create_tt_from_db_model(self):
-        model, tt_count = self.get_random_tt_from_db()
-        self.active_false(model)
-
-        return model.link, tt_count
+            return [self.create_model({"link": tt, "is_active": True}) for tt in tt_data]
 
 
 class GreetingManager(MediaManager):
-
-    @db_session
-    def get_random_greeting_from_db(self):
-        greetings = self.get_free_greeting_from_db()
-        return random.choice(greetings), len(greetings) - 1
-
-    @db_session
-    def get_free_greeting_from_db(self):
-        return list(select(greeting for greeting in Greeting if greeting.is_active))
+    """
+    Класс менеджер пожеланий.
+    """
+    model = Greeting
+    attr_name = "text"
 
     @db_session
     def greeting_model_create(self, text):
-        return Greeting(text=text, is_active=True)
-
-    @db_session
-    def create_greeting_from_db_model(self):
-        greeting, gr_count = self.get_random_greeting_from_db()
-        self.active_false(greeting)
-
-        return greeting.text, gr_count
+        """
+        Метод создания записи пожелания в бд.
+        :param text: текст пожелания.
+        :return: запись бд.
+        """
+        return self.create_model({"text": text, "is_active": True})
--- utils.py
+++ utils.py
@@ -12,7 +12,9 @@ from config import USER_NONE_ANSWER, MEM_MINIMUM_QUANTITY, TOKEN
 
 
 class BaseManager:
-
+    """
+    Класс базового менеджера
+    """
     def __init__(self):
         self.image_manager = ImageManager()
         self.tiktok_manager = TikTokManager()
@@ -21,10 +23,20 @@ class BaseManager:
         self.uploader = PhotoUploader(self.api.get_context())
 
     async def send_image(self, user_id, photo):
+        """
+        Метод отправки изображения
+        :param user_id: id пользователя
+        :param photo: файл фото
+        """
         await self.api.get_context().messages.send(user_id=user_id, attachment=photo,
                                                    random_id=random.choice(range(2 ** 20)))
 
     async def send_document(self, peer_id, link):
+        """
+        Метод отправки документа
+        :param peer_id: id чата
+        :param link: ссылка на документ
+        """
         doc = await DocUploader(self.api.get_context()).get_attachment_from_link(
             peer_id=peer_id,
             link=link,
@@ -33,6 +45,11 @@ class BaseManager:
                                                    random_id=random.choice(range(2 ** 20)))
 
     async def send_message(self, user_id, message):
+        """
+        Метод отправки сообщения
+        :param user_id: id пользователя
+        :param message: текст сообщения
+        """
         if isinstance(user_id, int):
             user_id = [user_id]
         await self.api.get_context().messages.send(user_ids=user_id, message=message,
@@ -40,9 +57,20 @@ class BaseManager:
 
     @db_session
     def get_admin_list(self):
+        """
+        Метод получения списка пользователей-админов.
+        :return: список пользователей админов
+        """
         return select(user for user in UserBot if user.status == 'admin')
 
     async def count_check(self, count, type):
+        """
+        Метод проверки количества объектов в бд и отправки сообщения администраторам, если их количество меньше
+        допустимого.
+        :param count: количество объектов.
+        :param type: тип объекта.
+        :return: сообщение
+        """
         if count > MEM_MINIMUM_QUANTITY:
             return
         user_id = [admin.user_id for admin in self.get_admin_list()]
@@ -51,13 +79,25 @@ class BaseManager:
 
     @staticmethod
     def get_link_from_message(text):
+        """
+        Метод получения ссылки из текста
+        :param text: текст
+        :return: ссылка
+        """
         return re.findall("(?P<url>https?://[^\s]+)", text)
 
 
 class Get(BaseManager):
-
+    """
+    Класс менеджера получения объектов из бд.
+    """
     @staticmethod
     async def help(user_id):
+        """
+        Метод отправки пользователю сообщения с help текстом.
+        :param user_id: id пользователя.
+        :return: сообщение пользователю.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user:
@@ -76,22 +116,36 @@ class Get(BaseManager):
                     return text
 
     async def image(self, user_id):
+        """
+        Метод получения изображения из бд и отправки его пользователю.
+        :param user_id: id пользователя.
+        :return: сообщение пользователю.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user:
-                mem_link, mem_count = self.image_manager.create_image_from_db_model()
-                photo = await self.uploader.get_attachment_from_link(peer_id=user_id,
-                                                                     link=mem_link)
+                mem_link, mem_count = self.image_manager.create_object_from_db_model()
+                if not mem_link:
+                    return "Произошла ошибка!"
+
+                photo = await self.uploader.get_attachment_from_link(peer_id=user_id, link=mem_link)
                 await self.send_image(user_id, photo)
                 await self.count_check(mem_count, 'мема')
             else:
                 return USER_NONE_ANSWER
 
     async def tt(self, user_id):
+        """
+        Метод получения тик тока из бд и отправки его пользователю.
+        :param user_id: id пользователя.
+        :return: сообщение пользователю.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user:
-                tt_link, tt_count = self.tiktok_manager.create_tt_from_db_model()
+                tt_link, tt_count = self.tiktok_manager.create_object_from_db_model()
+                if not tt_link:
+                    return "Произошла ошибка!"
 
                 await self.send_message(user_id, tt_link)
                 await self.count_check(tt_count, 'тик токов')
@@ -99,10 +153,17 @@ class Get(BaseManager):
                 return USER_NONE_ANSWER
 
     async def greeting(self, user_id):
+        """
+        Метод получения пожелания из бд и отправки его пользователю.
+        :param user_id: id пользователя.
+        :return: сообщение пользователю.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user:
-                gr_link, gr_count = self.greeting_manager.create_greeting_from_db_model()
+                gr_link, gr_count = self.greeting_manager.create_object_from_db_model()
+                if not gr_link:
+                    return "Произошла ошибка!"
 
                 await self.send_message(user_id, gr_link)
                 await self.count_check(gr_count, 'пожеланий')
@@ -111,41 +172,76 @@ class Get(BaseManager):
 
 
 class Add(BaseManager):
-
+    """
+    Класс менеджера добавления объектов в бд.
+    """
     async def image(self, user_id, event_attachments):
+        """
+        Метод добавления изображения в бд.
+        :param user_id: id пользователя.
+        :param event_attachments: вложение.
+        :return: сообщение администратору.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user and user.status == 'admin':
                 # get photos from message
                 attachments = [attachment.photo.sizes[-1] for attachment in event_attachments]
                 # send confirm message if image load to db
-                if self.image_manager.images_model_create(attachments):
-                    return 'Фото успешно загружены'
+                image_models = self.image_manager.image_models_create(attachments)
+                if image_models:
+                    if all(model is not None for model in image_models):
+                        return 'Все фото успешно загружены'
+                    elif all(model is None for model in image_models):
+                        return 'Ни одно фото не было загружено!'
+                    else:
+                        return 'Одно или несколько фото не было загружено!'
 
             return 'Что-то пошло не так. Возможно эти фото уже были загружены вами!'
 
     async def tt(self, text, user_id):
+        """
+        Метод добавления тик тока в бд.
+        :param text: текст, содержащий ссылку на тик ток.
+        :param user_id: id пользователя.
+        :return: сообщение администратору.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user and user.status == 'admin':
                 links = self.get_link_from_message(text)
-                self.tiktok_manager.tt_model_create(links)
-                return 'Тик токи успешно добавлены!'
+                tt_models = self.tiktok_manager.tt_models_create(links)
+                if tt_models:
+                    if all(tt is not None for tt in tt_models):
+                        return 'Все тик-токи были успешно добавлены!'
+                    elif all(tt is None for tt in tt_models):
+                        return 'Ни один тик-ток не был добавлен!'
+                    else:
+                        return 'Один или несколько тик-токов не был добавлен!'
 
             return 'Что-то пошло не так!'
 
     async def greeting(self, text, user_id):
+        """
+        Метод добавления пожелания в бд.
+        :param text: текст пожления.
+        :param user_id: id пользователя.
+        :return: сообщение администратору.
+        """
         with db_session():
             user = UserBot.get(user_id=user_id)
             if user and user.status == 'admin':
-                self.greeting_manager.greeting_model_create(text)
-                return 'Подзравление успешно добавлено!'
+                greeting = self.greeting_manager.greeting_model_create(text)
+                if greeting:
+                    return 'Поздравление успешно добавлено!'
 
             return 'Что-то пошло не так!'
 
 
 class CommandManager:
-
+    """
+    Менеджер команд.
+    """
     def __init__(self):
         self.add = Add()
         self.get = Get()
