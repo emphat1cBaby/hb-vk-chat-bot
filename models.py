bf8ff5fe6d6f8adbdd8875ecc6822bd3699e844b
Dima Smirnov <diamond101001@gmail.com>
2023-06-02 14:01:33 +0300

    update models.py for working with ripple

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
+import datetime
+
 from pony.orm import Database, Required
 from config import DB_CONFIG
 
 # psql \! chcp 1251 - смена кодировки
 db = Database()
 db.bind(**DB_CONFIG)
+# set_sql_debug(True)
+
+
+class TikTokMovies(db.Entity):
+    link = Required(str)
+    is_active = Required(bool)
 
 
 class PhotoMem(db.Entity):
@@ -13,19 +21,16 @@ class PhotoMem(db.Entity):
     is_active = Required(bool, sql_default=True)
 
 
+class Greeting(db.Entity):
+    text = Required(str)
+    is_active = Required(bool, sql_default=True)
+
+
 class UserBot(db.Entity):
     user_id = Required(int)
     status = Required(str)
-
-
-class TikTok(db.Entity):
-    link = Required(str)
-    is_active = Required(bool)
-
-
-class Greeting(db.Entity):
-    text = Required(str)
-    is_active = Required(bool)
+    ripple_status = Required(bool)
+    ripple_time = Required(datetime.datetime)
 
 
 db.generate_mapping(create_tables=True)
